37cc906a82aca7ef6ef0acd091f9ee1acadd0f39
Alexey@Work <ace@live.ru>
2024-03-27 10:32:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ReflectionTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = F.Get();
+            var serialized = string.Empty;
+            int testCount = 100000;
+
+            Console.WriteLine("Мой рефлекшен:");
+            serialized = MakeSerializationTest(f, testCount, SimpleCSVSerializer.Serialize);
+            MakeDeserializationTest(serialized, testCount, SimpleCSVSerializer.Deserialize<F>);
+
+            Console.WriteLine("Стандартный механизм (NewtonsoftJson):");
+            serialized = MakeSerializationTest(f, testCount, JsonConvert.SerializeObject);
+            MakeDeserializationTest(serialized, testCount, JsonConvert.DeserializeObject<F>);
+        }
+
+        static string MakeSerializationTest(object obj, int testCount, Func<object, string> serializeFunc)
+        {
+            var serialized = string.Empty;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < testCount; i++)
+            {
+                serialized = serializeFunc(obj);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Время на сериализацию: {stopwatch.ElapsedMilliseconds} ms");
+            return serialized;
+        }
+
+        static void MakeDeserializationTest(string serialized, int testCount, Func<string, object> deserializeFunc)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < testCount; i++)
+            {
+                var deserialized = deserializeFunc(serialized);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Время на десериализацию: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ SimpleCSVSerializer.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+public class SimpleCSVSerializer
+{
+    public static string Serialize(object obj)
+    {
+        var type = obj.GetType();
+        var sb = new StringBuilder();
+        foreach (var field in type.GetFields())
+        {
+            sb.Append($"{field.GetValue(obj)},");
+        }
+        foreach (var prop in type.GetProperties())
+        {
+            sb.Append($"{prop.GetValue(obj, null)},");
+        }
+        // Удаляем последнюю запятую
+        if (sb.Length > 0)
+            sb.Remove(sb.Length - 1, 1);
+        return sb.ToString();
+    }
+
+    public static T Deserialize<T>(string csv) where T : new()
+    {
+        T obj = new T();
+        var type = typeof(T);
+        string[] parts = csv.Split(',');
+        int index = 0;
+
+        foreach (var field in type.GetFields())
+        {
+            if (index < parts.Length)
+            {
+                field.SetValue(obj, Convert.ChangeType(parts[index], field.FieldType));
+                index++;
+            }
+        }
+
+        foreach (var prop in type.GetProperties())
+        {
+            if (index < parts.Length && prop.CanWrite)
+            {
+                prop.SetValue(obj, Convert.ChangeType(parts[index], prop.PropertyType), null);
+                index++;
+            }
+        }
+
+        return obj;
+    }
+}
